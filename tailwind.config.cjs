fa0c9bf553eddb47402e34a2faf43ba86753444a
Èrik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-10-12 01:14:34 -0300

    feat: add font Inter as default

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx'
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      },
+    },
   },
   plugins: [],
 }
