428151ceb63e0a41d59c2d719430c19ca9be6539
Èrik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-10-13 19:41:38 -0300

    fix(tailwind): make tailwind see html extensions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: ['./*.html', './src/**/*.tsx'],
   theme: {
     fontSize: {
       xs: 14,
